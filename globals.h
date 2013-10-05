3210a5c2a99e29b1cd4d56c1c59096c90322afb4
Aviv Beeri <avbeeri@gmail.com>
2013-10-05 19:14:45 +0100

    Moved the globals to an external file to include

---
+++ globals.h
@@ -0,0 +1,11 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+
+enum KEY { UP, DOWN, LEFT, RIGHT , ESCAPE, KEY_A, KEY_S, KEY_D, KEY_F};
+
+const float FPS = 60;
+const int HEIGHT = 540;
+const int WIDTH = 1010;
+
+#endif
