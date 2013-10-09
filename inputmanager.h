aee592b78ae0a0845f30020db4c73d958e640775
Aviv Beeri <avbeer@gmail.com>
2013-10-09 23:50:53 +0100

    Did some name changed, added a constructor to the inputmanager to ensure the keyboard is loaded before attempting to gain its state.

--- inputmanager.h
+++ inputmanager.h
@@ -1,16 +1,19 @@
 #ifndef INPUTMANAGER_H
 #define INPUTMANAGER_H
 
+#include <stdio.h>
 #include <allegro5/allegro.h>
+#include <allegro5/allegro_primitives.h>
 #include "globals.h"
 
 class InputManager {
 
   public:
-    bool isKeyDown(KEY key);
+    InputManager();
+    bool isPressed(KEY key);
     void onEvent(ALLEGRO_EVENT ev);
   private:
-    ALLEGRO_KEYBOARD_STATE  state; 
+    ALLEGRO_KEYBOARD_STATE key_state; 
 };
 
 #endif
