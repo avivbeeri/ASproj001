dab0cd3c2fc09e307560e4a34752d7ba9afe9369
Aviv Beeri <avbeeri@gmail.com>
2013-10-16 14:24:26 +0100

    added a constructor so tuples can be built from a string.

--- tuple.h
+++ tuple.h
@@ -1,11 +1,16 @@
 #ifndef TUPLE_H
 #define TUPLE_H
 
+#include <string>
 #include "globals.h"
 
+
+using std::string;
 class Tuple {
-  public:
+  
+	public:
     Tuple(KEY, KEY, KEY, KEY);
+    Tuple(string data);
 	  KEY getElement(int i);
     void output();
   private:
