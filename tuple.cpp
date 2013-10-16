1d09a4a3048feca909cff9a64ce79fa7efb10e7c
Aviv Beeri <avbeeri@gmail.com>
2013-10-16 14:28:46 +0100

    Fixed another bug in tuple, but multiple bars do work.

--- tuple.cpp
+++ tuple.cpp
@@ -9,22 +9,22 @@ Tuple::Tuple(KEY k0, KEY k1, KEY k2, KEY k3) {
 }
 
 Tuple::Tuple(string lineData) {
-  if (!lineData.at(0) == '0') {
+  if (!(lineData.at(0) == '0')) {
     element[0] = LEFT;
 	} else {
     element[0] = EMPTY;
 	}
-  if (!lineData.at(1) == '0') {
+  if (!(lineData.at(1) == '0')) {
     element[1] = UP;
 	} else {
     element[1] = EMPTY;
 	}
-  if (!lineData.at(2) == '0') {
+  if (!(lineData.at(2) == '0')) {
     element[2] = DOWN;
 	} else {
     element[2] = EMPTY;
 	}
-  if (!lineData.at(3) == '0') {
+  if (!(lineData.at(3) == '0')) {
     element[3] = RIGHT;
 	} else {
     element[3] = EMPTY;
